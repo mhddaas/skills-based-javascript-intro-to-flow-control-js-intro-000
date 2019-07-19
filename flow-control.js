9f8fce9d134979629c125dabbe4bbc7d20bd7abe
mahmoud Al Daas <s15c5c@umwelt-campus.de>
2019-07-19 13:41:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,21 +5,58 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  {
+    if (age >= 13 && age <= 19)
+    {
+      return "You are a teenager!";
+    }
+    else
+    {
+      return "You are not a teenager";
 }
 
 function ageChecker(age) {
-
+  {
+    if (age >= 13 && age <= 19)
+    {
+      return "You are a teenager!";
+    }
+    else if (age < 13)
+    {
+      return "You are a kid";
+    }
+    else if (age > 19)
+    {
+      return "You are a grownup";
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager";
+  }
+  else
+  {
+    return "You are not a teenager";
+
 
 }
 
 function switchAge(age) {
-  switch (age)
-  case ("age" )
-  return "you are a teenager"
+  switch(age)
+  {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    return "You are a teenager";
+    break;
 
+    default:
+    return "You have an age";
+    break;
+  }
 
 }
